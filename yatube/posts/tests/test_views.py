1e21e12da7e245f0c54e2c8b875651a5d8ddebd3
Olesya <lesenysh@yandex.ru>
2021-11-12 12:44:57 +0300

    Спринт 6

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.cache import cache
 from django.contrib.auth import get_user_model
-from django.http import response
 from django.test import Client, TestCase
 from django.urls import reverse
 
